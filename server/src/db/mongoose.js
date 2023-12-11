bee775d4e9ce3840fd6c694202b5021cbfd78175
zeshan <zeshanahmedoff@gmail.com>
2023-12-11 22:19:52 +0500

    env secret moved

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URL ||
-    'mongodb+srv://tg-admin:admin@cluster0.nltdz.mongodb.net/task-manager?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+});
